5cda7cd8b1b428b696a74bb4d449e1a6c6c61746
Theresa Henderson <thenderson@squaxin.us>
2014-11-19 10:17:29 -0800

    updated homework

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,25 @@
 //Function called "signIn", causes an alert to pop up with text "Thanks for signing in". Should be triggered by clicking on the button with id of "thanksIn".
 
-function signIn() { 
-    document.getElementById("thanksIn").innerHTML = "Thanks for Signing In!";
+function signIn() 
+{ 
+	alert("Thanks for signing in.");
 }
-
 
 
 
 //Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "thanksUp".
 
-function signUp() { 
-    document.getElementById("thanksUp").innerHTML = "Thanks for Signing Up";
-}
-
-function getUserInput() {
+function signUp() 
+{
     var userInput = prompt("Email address please");
-    document.getElementById("output-here").innerHTML = "Thanks for signing up"; userInput; 
+    document.getElementById("output-here").innerHTML = "Thanks for signing up " + userInput; 
 }
 
 
 
 //Function called "extraCredit", causes an alert to pop up with text "Thanks for doing extra credit". Should be triggered by clicking on the button with id of "extraCredit".
 
-function extraCredit() { 
+function extraCredit() 
+{ 
     document.getElementById("extraCredit").innerHTML = "Thanks for doing extra credit";
 }
